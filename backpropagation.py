4b5c1957645494ce3783c1cf347b8d1137eaefec
lucaschv <lcs.chv@gmail.com>
2017-05-11 18:29:07 -0300

    Backpropagation complete.

--- backpropagation.py
+++ backpropagation.py
@@ -3,6 +3,7 @@ import argparse
 import sys
 # from src.input import Input
 from src.neuralnet import NeuralNet
+
 __author__ = 'Lucas Chaves'
 
 
@@ -11,30 +12,36 @@ def get_args():
 	parser = argparse.ArgumentParser(description='Realize the hand-written digits recognition.')
 	#Add arguments to the program
 	parser.add_argument(
-		"-i", "--input", help="Directs the input to the dataset of your choice", required=True)
+		"-i", "--input", help="Directs the input to the dataset of your choice.", required=True)
 	parser.add_argument(
-		"-o", "--output", help="Directs the output to a name of your choice", required=True)
+		"-o", "--output", help="Directs the output to a name of your choice.", required=True)
 	parser.add_argument(
-		"-a", "--algorithm", help="Select the algorithm strategy that will be used to calculate the gradient", required=True)
+		"-b", "--batch_size", type=int, help="Select the algorithm strategy by chosing the batch_size. Ps.: set 1 to SGD or 5000 to GD.", required=True)
+	parser.add_argument(
+		"-lr", "--learning_rate",type=float, help="Select the learning_rate that you want to use.", required=True)
+	parser.add_argument(
+		"-n", "--num_neurons", type=int, help="Select the number of neurons in the hidden layer.", required=True)
+	parser.add_argument(
+		"-e", "--ephocs", type=int, help="Number of epochs that you want to compute.", required=True)
 	#Array containing all the prguments passed to the program
 	args = parser.parse_args()
 	#Assign args to variables
 	input_file = args.input
 	output_file = args.output
-	algorithm = args.algorithm
+	batch_size = args.batch_size
+	learning_rate = args.learning_rate
+	num_neurons = args.num_neurons
+	ephocs = args.ephocs
+
 	
 	#return variables
-	return input_file, output_file, algorithm
+	return input_file, output_file, batch_size, learning_rate, num_neurons, ephocs
 
 
 def main():
-	filein, fileout, algorithm = get_args()
+	filein, fileout, batch_size, learning_rate, num_neurons, ephocs = get_args()
 	
-	neuralnet = NeuralNet(filein)
-
-	neuralnet.setOutputUnits()
-	output_file = open(fileout, 'w')
-
+	neuralnet = NeuralNet(filein, fileout, batch_size, learning_rate, num_neurons, ephocs)
 
 if __name__ == "__main__":
 	main()
--- src/neuralnet.py
+++ src/neuralnet.py
@@ -1,112 +1,76 @@
 #!/usr/bin/python
 import numpy as np
-# from input import Input
 import math
+from sklearn.preprocessing import StandardScaler
+from scipy.special import expit
+import pandas as pd
+from sklearn.metrics import log_loss
 
 class NeuralNet(object):
-	"""docstring for NeuralNet"""
-	
-	labels = np.zeros((5000,10), dtype=np.int)
 
- 
-	def __init__(self, filein):
+	def __init__(self, filein, fileout, batch_size, learning_rate, num_neurons, ephocs):
 		super(NeuralNet, self).__init__()
-		self.setOutputUnits()
-		self.readTrainSet(filein)
-		self.createOutWeightsMatrix(25)
-		self.createWeightMatrix(25)
-		self.createInputUnits(filein,1)
-		
-				
-		# self.arg = arg
-	def setOutputUnits(self):
-		self.outUnit = np.identity(10)
+		self.filein = filein
+		self.fileout = fileout
+		self.batch_size = batch_size
+		self.lr = learning_rate
+		self.numNeurons = num_neurons
+		self.ephocs = ephocs
 
-	def createInputUnits(self,filein, batchsize):
-		# self.input = np.zeros((5,1), dtype=np.float)
-		self.lr = 0.5
-		# self.input = np.random.randint(9, size=(batchsize, 784))
-		self.input = np.empty([1, 784])
+		self.readTrainSet()
+		self.createWeightMatrix()
+		self.BackpropagationAlgorithm()
+
+	def BackpropagationAlgorithm(self):
+
+		output_file = open(self.fileout, 'w')
 		cont = 0
-		for x in xrange(1,60):
-			input_file = open(filein,'r')
-			for i in input_file:
-				i = i.rstrip()
-				vet = i.split(",")
-				self.label = int(vet[0])
-				self.input = map(int, vet[1:])		
-				# print self.label
-				print cont
-				cont +=1
-				self.calcForwardPass()
-				print "GroundTruth" ,self.outUnit[self.label]
-				print "Probabilities of Z", self.z
-				self.derror = np.subtract(self.outUnit[self.label],self.z)
-				print "Error", self.derror
-				self.hidden_neurons_error()
-				self.updateWeights()
-				self.updateOutWeightsMatrix()
+		for epoch in xrange(self.ephocs):
 
-	def hidden_neurons_error(self):
-		self.neurons_err = np.dot(self.weightOut, self.derror)
-		# print "hidden errors", self.neurons_err
+			for batch in range(0,self.input.shape[0], self.batch_size):
+				input_slice = self.input[batch:batch+self.batch_size]
 
-	def updateWeights(self):
-		# print self.input[0]
-		# print "\n"
-		for i in range(0,784):
-			for j in range(0,self.numNeurons):
-				# print "parameters: ",self.weights[i][j], self.lr, self.neurons_err[j], self.dsigmoid(self.input[i])
-				self.weights[i][j] = self.weights[i][j] + (self.lr * self.neurons_err[j]) * self.input[j]
-				# print "WeightAfter: ",self.weights[i][j]
+				self.calcForwardPass(input_slice)
+				self.calculateError(batch)
+				self.updateWeights(input_slice)
+			self.calcEpochErr(epoch)	
 
-	def updateOutWeightsMatrix(self):
-		for i in range(0,self.numNeurons):
-			for j in range(0,10):
-				self.weightOut[i][j] = self.weightOut[i][j] + (self.lr * self.derror[j]) * self.y[j]
+	def calcEpochErr(self, epoch):
+		l1_test = expit(np.dot(self.input,self.weights))
+		l2_test = expit(np.dot(l1_test,self.weightsOut))
+		print("Cross Entropy empirical error at epoch "+str(epoch)+": "+str(log_loss(self.labels,l2_test)))
 
-	def createWeightMatrix(self, numNeurons):
-		self.numNeurons = numNeurons
-		self.derror = np.empty([10, 1])
+	def calculateError(self, batch):
+		self.derror = self.r_expected[batch:batch+self.batch_size] - self.l2
+		# self.l2_delta = self.derror 		
+		self.hidder_error = self.derror.dot(self.weightsOut.T) * self.sigmoid_prime(self.l1)
 
-		self.weights = np.random.uniform(-4,4,[784,numNeurons])
+	def updateWeights(self, input_slice):
+		self.weightsOut+= self.lr * (self.l1.T.dot(self.derror))
+		self.weights+= self.lr * (input_slice.T.dot(self.hidder_error))
 
-	def createOutWeightsMatrix(self, numNeurons):
-		self.weightOut = np.random.uniform(-4,4,[numNeurons,10])
-	
+	def createWeightMatrix(self):
+		self.weights = np.random.uniform(-1,1,[self.input.shape[1],self.numNeurons])
+		self.weightsOut = np.random.uniform(-1,1,[self.numNeurons,10])
 	
 	### self.y corresponds to the results of the neurons of the hidden layer
 	### self.z corresponds to the the prediction of the forward pass, it will result in a 10 positions vector with the respectives probabilities estimated for each digit
-	def calcForwardPass(self):
-		self.y = np.dot(self.input, self.weights)
-		# print self.y
-		self.y = self.sigmoid(self.y)
-		# print self.y
-		self.z = np.dot(self.y, self.weightOut)
-		self.z = self.sigmoid(self.z)
-		# print self.z
-
-	def sigmoid(self,x):
-		return 1 / (1 + np.exp(-x))	
-
-	# def sigmoid(self, signal):
-	#     # Prevent overflow.
-	#     signal = np.clip( signal, -500, 500 )
-	#     # Calculate activation signal
-	#     signal = 1.0/( 1 + np.exp( -signal ))
-	#     return signal
-
 	
-	
-	def sigmoid_prime(z):
-		"""Derivative of the sigmoid function."""
-		return sigmoid(z)*(1-sigmoid(z))
+	def calcForwardPass(self, X_batch):				
+		self.l1 = expit(np.dot(X_batch,self.weights))
+		self.l2 = expit(np.dot(self.l1,self.weightsOut))
 
-	def readTrainSet(self, filein):
-		input_file = open(filein, 'r')
+	def sigmoid_prime(self,z):
+		return z * (1-z)
+
+	def readTrainSet(self):
+		MNIST_dataset = pd.read_csv(self.filein,header=None,sep=",").rename(columns={0: "label"})
 		cont = 0
-		for i in input_file:
-			i = i.rstrip()
-			vet = i.split(",")
-			self.labels[cont][int(vet[0])] = 1
-			cont += 1
+		self.labels = MNIST_dataset["label"]
+		self.r_expected = np.zeros((self.labels.shape[0],10), dtype=np.int)
+		for lab in self.labels:
+			self.r_expected[cont][int(lab)] = 1
+			cont+=1
+		self.input = MNIST_dataset[[col for col in MNIST_dataset.columns if col !="label"]]
+		self.input = StandardScaler().fit_transform(self.input) #scaling features
+		self.input = np.c_[np.ones(self.input.shape[0]), self.input] #adding biases
