cbfdc5d07e6a80e5f1f54bdfa8b27b3aeadc490c
lucaschv <lcs.chv@gmail.com>
2017-05-13 17:00:57 -0300

    Cleaning some code..

--- src/neuralnet.py
+++ src/neuralnet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import numpy as np
+from sklearn import preprocessing
 import math
-from sklearn.preprocessing import StandardScaler
 from scipy.special import expit
 import pandas as pd
 from sklearn.metrics import log_loss
@@ -18,16 +18,14 @@ class NeuralNet(object):
 		self.ephocs = ephocs
 
 		self.readTrainSet()
-		self.createWeightMatrix()
+		self.createNet()
 		self.BackpropagationAlgorithm()
 
 	def BackpropagationAlgorithm(self):
 
-		output_file = open(self.fileout, 'w')
-		cont = 0
 		for epoch in xrange(self.ephocs):
 
-			for batch in range(0,self.input.shape[0], self.batch_size):
+			for batch in range(0,len(self.input), self.batch_size):
 				input_slice = self.input[batch:batch+self.batch_size]
 
 				self.calcForwardPass(input_slice)
@@ -41,36 +39,38 @@ class NeuralNet(object):
 		print("Cross Entropy empirical error at epoch "+str(epoch)+": "+str(log_loss(self.labels,l2_test)))
 
 	def calculateError(self, batch):
-		self.derror = self.r_expected[batch:batch+self.batch_size] - self.l2
-		# self.l2_delta = self.derror 		
-		self.hidder_error = self.derror.dot(self.weightsOut.T) * self.sigmoid_prime(self.l1)
+		self.derror = self.r_expected[batch:batch+self.batch_size] - self.z
+		self.hidder_error = np.dot(self.derror, np.transpose(self.weightsOut)) * self.sigmoid_prime(self.y)
 
 	def updateWeights(self, input_slice):
-		self.weightsOut+= self.lr * (self.l1.T.dot(self.derror))
-		self.weights+= self.lr * (input_slice.T.dot(self.hidder_error))
+		self.weightsOut+= self.lr * np.dot(np.transpose(self.y),self.derror)
+		self.weights+= self.lr * np.dot(np.transpose(input_slice), self.hidder_error)
 
-	def createWeightMatrix(self):
-		self.weights = np.random.uniform(-1,1,[self.input.shape[1],self.numNeurons])
+	def createNet(self):
+		self.input = preprocessing.scale(self.input)
+		bias = np.ones((len(self.input)))
+		self.input = np.c_[bias, self.input]
+		self.weights = np.random.uniform(-1,1,[len(self.input[0]),self.numNeurons])
 		self.weightsOut = np.random.uniform(-1,1,[self.numNeurons,10])
+		output_file = open(self.fileout, 'w')
+		
 	
+
 	### self.y corresponds to the results of the neurons of the hidden layer
 	### self.z corresponds to the the prediction of the forward pass, it will result in a 10 positions vector with the respectives probabilities estimated for each digit
-	
 	def calcForwardPass(self, X_batch):				
-		self.l1 = expit(np.dot(X_batch,self.weights))
-		self.l2 = expit(np.dot(self.l1,self.weightsOut))
+		self.y = expit(np.dot(X_batch,self.weights))
+		self.z = expit(np.dot(self.y,self.weightsOut))
 
 	def sigmoid_prime(self,z):
 		return z * (1-z)
 
 	def readTrainSet(self):
-		MNIST_dataset = pd.read_csv(self.filein,header=None,sep=",").rename(columns={0: "label"})
-		cont = 0
-		self.labels = MNIST_dataset["label"]
+		MNIST_dataset = pd.read_csv(self.filein,header=None,sep=",").rename(columns={0: "lab"})
+		self.labels = MNIST_dataset["lab"]
 		self.r_expected = np.zeros((self.labels.shape[0],10), dtype=np.int)
+		cont = 0
 		for lab in self.labels:
 			self.r_expected[cont][int(lab)] = 1
 			cont+=1
-		self.input = MNIST_dataset[[col for col in MNIST_dataset.columns if col !="label"]]
-		self.input = StandardScaler().fit_transform(self.input) #scaling features
-		self.input = np.c_[np.ones(self.input.shape[0]), self.input] #adding biases
+		self.input = MNIST_dataset[[col for col in MNIST_dataset.columns if col !="lab"]]
